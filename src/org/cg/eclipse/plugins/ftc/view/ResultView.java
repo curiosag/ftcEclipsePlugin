7027c59e53024cef2a90539bc755cfb63ad462b4
curiosag <curiosa.globunznik@gmail.com>
2016-11-07 20:28:48 +0100

    result view with copy to clipboard

--- src/org/cg/eclipse/plugins/ftc/view/ResultView.java
+++ src/org/cg/eclipse/plugins/ftc/view/ResultView.java
@@ -1,6 +1,7 @@
 package org.cg.eclipse.plugins.ftc.view;
 
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.part.*;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Table;
@@ -11,9 +12,15 @@ import javax.swing.table.TableModel;
 
 import org.cg.eclipse.plugins.ftc.PluginConst;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.dnd.Clipboard;
+import org.eclipse.swt.dnd.TextTransfer;
+import org.eclipse.swt.dnd.Transfer;
+import org.eclipse.swt.events.KeyEvent;
+import org.eclipse.swt.events.KeyListener;
 
 public class ResultView extends ViewPart {
 
+	Clipboard clipboard;
 	public static final String ID = PluginConst.RESULT_VIEW_ID;
 	private Table table;
 
@@ -27,21 +34,24 @@ public class ResultView extends ViewPart {
 
 		GridData data = new GridData(SWT.FILL, SWT.FILL, true, true);
 		data.heightHint = 200;
+
 		table.setLayoutData(data);
 
+		clipboard = new Clipboard(Display.getDefault());
+		table.addKeyListener(createKeyListener());
 	}
 
 	public void displayTable(TableModel model) {
-		
+
 		table.setRedraw(false);
 		table.removeAll();
-		while ( table.getColumnCount() > 0 ) {
-		    table.getColumns()[ 0 ].dispose();
+		while (table.getColumnCount() > 0) {
+			table.getColumns()[0].dispose();
 		}
 		table.setRedraw(true);
-		
+
 		int colCount = model.getColumnCount();
-		
+
 		for (int i = 0; i < colCount; i++) {
 			TableColumn column = new TableColumn(table, SWT.NONE);
 			column.setText(model.getColumnName(i));
@@ -57,9 +67,33 @@ public class ResultView extends ViewPart {
 			table.getColumn(i).pack();
 	}
 
-	@Override
-	public void setFocus() {
+	private KeyListener createKeyListener() {
 
+		return new KeyListener() {
+
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if (((e.stateMask & SWT.CTRL) == SWT.CTRL) && (e.keyCode == 'c'))
+					clipboard.setContents(new Object[] { getSelectionText() },
+							new Transfer[] { TextTransfer.getInstance() });
+			}
+
+			private Object getSelectionText() {
+				String result = "";
+				TableItem[] selection = table.getSelection();
+				for (int i = 0; i < selection.length; i++)
+					result += selection[i].getText() + " ";
+				return result;
+			}
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+			}
+		};
 	}
 
+	@Override
+	public void setFocus() {
+	}
+	
 }
